003577707cb969a61e6b3a3728b04911f3f710d1
Patrick Li <patrli@umich.edu>
2024-02-07 23:08:39 -0500

    functor

---
+++ Lab4/functor.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Functor {
+  public:
+    void operator()() { cout << "Functor Called" << endl; }
+};
+
+int main() {
+    cout << "Operation 1" << endl;
+    auto f = Functor();
+    cout << "Operation 2" << endl;
+    f();
+}
